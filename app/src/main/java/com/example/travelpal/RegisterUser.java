6e7982460808394cc1f184c9dd54f24a6ca34bc9
Amandeep <c0850554@mylambton.ca>
2022-07-28 14:27:39 -0400

    Working On Elements Of Register Activity

--- app/src/main/java/com/example/travelpal/RegisterUser.java
+++ app/src/main/java/com/example/travelpal/RegisterUser.java
@@ -3,12 +3,19 @@ package com.example.travelpal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterUser extends AppCompatActivity {
 
+    EditText Reg_Fn,Reg_Ln,Reg_Email,Reg_Pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        Reg_Fn=findViewById(R.id.reg_firstname);
+        Reg_Ln=findViewById(R.id.reg_lastname);
+        Reg_Email=findViewById(R.id.reg_email);
+        Reg_Pass=findViewById(R.id.reg_password);
+
     }
 }
