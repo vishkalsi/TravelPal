f8a332d2efc62b32e655fdea15ea18d4cbaff5e1
Amandeep <c0850554@mylambton.ca>
2022-07-28 18:33:10 -0400

    Login Ui Modified

--- app/src/main/java/com/example/travelpal/Login.java
+++ app/src/main/java/com/example/travelpal/Login.java
@@ -15,22 +15,22 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView textUsername = (TextView) findViewById(R.id.textUsername);
-        TextView textPassword = (TextView) findViewById(R.id.textPassword);
+       // TextView textUsername = (TextView) findViewById(R.id.textUsername);
+        //TextView textPassword = (TextView) findViewById(R.id.textPassword);
 
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
-
-        loginBtn.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                if(textUsername.getText().toString().equals("admin") && textPassword.getText().toString().equals("admin")) {
-                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(Login.this, "Login Failed !!!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+//
+//        loginBtn.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view) {
+//                if(textUsername.getText().toString().equals("admin") && textPassword.getText().toString().equals("admin")) {
+//                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(Login.this, "Login Failed !!!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
 
     }
 
