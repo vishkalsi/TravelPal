78ad3ec707283a394bf53669162c5c46063e4bc3
Aastha K <105570835+aastha852@users.noreply.github.com>
2022-07-29 15:19:32 -0400

    SplashScreen UI

--- app/src/main/java/com/example/travelpal/SplashScreen.java
+++ app/src/main/java/com/example/travelpal/SplashScreen.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class Splash extends AppCompatActivity {
-Handler handler;
+public class SplashScreen extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        handler=new Handler();
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(Splash.this,Login.class);
+                Intent intent=new Intent(SplashScreen.this,Login.class);
                 startActivity(intent);
                 finish();
             }
