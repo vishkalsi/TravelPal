cbe7c04c124e9d1dbe4f78c61bc0ef49ddb5b105
Aastha <aasthakhaneja623@gmail.com>
2022-07-30 15:39:20 -0400

    trial commit

--- app/src/main/java/com/example/travelpal/HomeFeed.java
+++ app/src/main/java/com/example/travelpal/HomeFeed.java
@@ -11,5 +11,6 @@ public class HomeFeed extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_feed);
 //        change1
+//        change2
     }
 }
